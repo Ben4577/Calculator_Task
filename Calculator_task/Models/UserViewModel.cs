1d3b9e61e1c708869afa826a528c006f04d83ea0
ben kellington <ben4577@hotmail.co.uk>
2023-07-13 17:58:38 +0100

    remove not needed models

--- Calculator_task/Models/UserViewModel.cs
+++ Calculator_task/Models/UserViewModel.cs
@@ -7,22 +7,8 @@ namespace Pharmacy2U_task.Models
 {
     public class UserViewModel
     {
-        //public string firstNumber { get; set; }
-        //public string secondNumber { get; set; }
-
         public double firstNum { get; set; }
         public double secondNum { get; set; }
         public double resultFromCalc { get; set; }
-
-
-  //      firstNumber: string;
-  //secondNumber: string;
-
-  //firstNum: number;
-  //secondNum: number;
-
-  //result: string = "";
-  //error: string = "";
-
     }
 }
